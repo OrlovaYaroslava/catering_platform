adfe4e5e3e5b27543b3fef8b198cec284ee04c63
OrlovaYaroslava <orlovayaroslawa@yandex.ru>
2026-01-28 16:04:49 +0700

    добавлены функции входа и выхода

--- app/auth.py
+++ app/auth.py
@@ -1,14 +1,26 @@
-from flask import Blueprint, render_template, redirect, url_for
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from app.models import User, Role
-from app.forms import RegisterForm
+from app.forms import RegisterForm, LoginForm
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return "Login page (temporary)"
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user and check_password_hash(user.password_hash, form.password.data):
+            login_user(user)
+            return redirect(url_for("main.index"))
+        else:
+            flash("Invalid email or password")
+
+    return render_template("login.html", form=form)
 
 @auth.route("/register", methods=["GET", "POST"])
 def register():
@@ -29,3 +41,9 @@ def register():
         return redirect(url_for("auth.login"))
 
     return render_template("register.html", form=form)
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
