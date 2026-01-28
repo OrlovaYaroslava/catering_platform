401a34a920ca335d441d1cca0d7ee99f39689d41
OrlovaYaroslava <orlovayaroslawa@yandex.ru>
2026-01-28 15:49:14 +0700

    добавлен регистрация клиента

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
+from werkzeug.security import generate_password_hash
+from app import db
+from app.models import User, Role
+from app.forms import RegisterForm
 
 auth = Blueprint("auth", __name__)
+
+@auth.route("/login")
+def login():
+    return "Login page (temporary)"
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        client_role = Role.query.filter_by(name="client").first()
+
+        user = User(
+            email=form.email.data,
+            password_hash=generate_password_hash(form.password.data),
+            role=client_role
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("auth.login"))
+
+    return render_template("register.html", form=form)
---
+++ app/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class RegisterForm(FlaskForm):
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()]
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired(), Length(min=6)]
+    )
+
+    confirm_password = PasswordField(
+        "Confirm password",
+        validators=[DataRequired(), EqualTo("password")]
+    )
+
+    submit = SubmitField("Register")
